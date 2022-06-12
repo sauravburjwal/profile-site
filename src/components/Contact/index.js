810949af52a69181301571e71e20e4933babdb60
Saurav Burjwal <sauravburjwal@gmail.com>
2022-06-12 16:26:01 +0530

    profile site with home, about, portfoolio,contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,69 @@
+import Loader from 'react-loaders'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const Contact = () => {
+    const [letterClass,setLetterClass] = useState('text-animate')
+    useEffect(()=> {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)}
+    ,[])
+  return (
+    <>
+    <div className='container contact-page'>
+        <div className='text-zone'>
+            <h1>
+                <AnimatedLetters letterClass={letterClass} strArray={['C','o','n','t','a','c','t',' ','M','e']} idx={15}/>
+            </h1>
+            <p>
+            I am interested in working opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className='contact-form'>
+              <form>
+                  <ul>
+                      <li className='half'>
+                          <input type="text" name='name' placeholder='Name' required/>
+                      </li>
+                      <li className='half'>
+                          <input type="email" name='email' placeholder='Email' required/>
+                      </li>
+                      <li>
+                          <input placeholder='Subject' type='text' name='subject' required/>
+                      </li>
+                      <li>
+                          <textarea placeholder='Message' name='message' required/>
+                      </li>
+                      <li>
+                          <input type='submit' className='flat-button' value='SEND'/>
+                      </li>
+                  </ul>
+              </form>
+          </div>
+        </div>
+        <div className='info-map'>
+            Saurav Burjwal <br/>
+            India <br/>
+            Bapu Dham,  Sarsol <br/>
+            Aligarh
+            <span>example@gmail.com</span>
+         </div>
+         <div className='map-wrap'>
+             <MapContainer center={[27.88590,78.05446]} zoom={12}>
+                 <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+                 <Marker position={[27.88590,78.05446]}>
+                     <Popup>Saurav lives here,come for a cup of Tea.</Popup>                     
+                 </Marker>                                  
+             </MapContainer>
+         </div>
+    </div>
+    <Loader type='pacman'/>
+    </>
+  )
+}
+
+export default Contact
